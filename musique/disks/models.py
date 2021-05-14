699fa06e5b8f9187db5eda57bbe46cbe959e1c63
gabrielhayoun <78904699+gabrielhayoun@users.noreply.github.com>
2021-05-14 20:19:24 +0200

    première partie du projet : affichage de la page d'accueil avec les albums et les artistes

--- musique/disks/models.py
+++ musique/disks/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Track(models.Model):
+    name = models.CharField(max_length=200)
+    composer = models.CharField(max_length=50)
+    milliseconds = models.IntegerField(verbose_name='time')
+    bytes = models.IntegerField()
+    unitPrice = models.FloatField()
+    album = models.ForeignKey('Album', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Track'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Album(models.Model):
+    title= models.CharField(max_length=200)
+    artist = models.ForeignKey('Artist', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Album'
+        ordering = ['artist']
+
+    def __str__(self):
+        return self.title
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
--- musique/disks/views.py
+++ musique/disks/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Album
 
 # Create your views here.
+
+def albums(request):
+    return render(request, 'disks/albums.html', {'list_albums': Album.objects.all()})
